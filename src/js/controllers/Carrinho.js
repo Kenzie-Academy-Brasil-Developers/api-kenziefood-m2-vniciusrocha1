53a6ce043f5c7164c33a6f5d50f0dbf62a33a4e5
andersonStorqui <andersoncxszn@gmail.com>
2022-01-25 10:50:50 -0300

    controller Carrinho Feito

--- src/js/controllers/Carrinho.js
+++ src/js/controllers/Carrinho.js
@@ -1,7 +1,45 @@
-import {KenzieFoodControll} from "./KenzieFood.js";
+import {KenzieFoodControll} from "./KenzieFood.js"; 
 class CarrinhoControll{
-    static async criandoDOM(id){
-        //pegar id, ai construir o DOM
+    static async addCarrinho(id){
+        if(id !== undefined){
+            const getDoProduto = KenzieFoodControll.get(id);
+            const produto = await getDoProduto;
+            const readCarrinho = this.read()
+            const carrinho = (readCarrinho.length > 0)? readCarrinho : [] ;
+            carrinho.push(produto)
+            this.write(carrinho);
+            this.criandoDOM()
+        }
+    }
+    static read() {
+        return JSON.parse(localStorage.getItem('carrinho'))
+    }
+    static write(obj) {
+        localStorage.setItem('carrinho', JSON.stringify(obj));
+    }
+    static criandoDOM() {
+        const listagem   = document.querySelector(".list__carrinho");
+        listagem.innerHTML = ""
+        const carrinho = this.read()
+        if(carrinho.length > 0){
+            carrinho.forEach((item) => {
+                const {categoria, nome, preco, imagem, id} = item
+                const li         = document.createElement('li');
+                const categoriaP = document.createElement('p');
+                const nomeh3     = document.createElement('h3');
+                const precoP     = document.createElement('p');
+                const imgSrc        = document.createElement('img')
+                categoriaP.innerText = categoria;
+                nomeh3.innerText      = nome;
+                precoP.innerText     = preco
+                imgSrc.src          = imagem
+                li.appendChild(imgSrc);
+                li.appendChild(nomeh3);
+                li.appendChild(categoriaP);
+                li.appendChild(precoP);
+                listagem.appendChild(li)
+            })
+        }
     }
 }
 export {CarrinhoControll}
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,2 @@
 import {VitrineControll} from "./controllers/Vitrine.js";
 import {CarrinhoControll} from "./controllers/Carrinho.js";
-import{ KenzieFoodControll } from "./controllers/KenzieFood.js"
-
-KenzieFoodControll.get(2)
-// console.log('KenzieFoodControll.get()', KenzieFoodControll.get())
-
