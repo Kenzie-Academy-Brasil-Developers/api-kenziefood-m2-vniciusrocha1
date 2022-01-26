ac1975c9d7be78d3a7b61986333abe10351ac184
Vinicius Rocha <vinicius.rocha.mobile@gmail.com>
2022-01-25 23:15:14 -0400

    Trocando o endpoint para testar o pages

--- src/js/controllers/KenzieFood.js
+++ src/js/controllers/KenzieFood.js
@@ -2,8 +2,8 @@ import {produtos} from "../mock/produtos.js";
 class KenzieFoodControll {
     static token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjQzMDQ0MjIwLCJleHAiOjE2NDM5MDgyMjAsInN1YiI6IltvYmplY3QgVW5kZWZpbmVkXSJ9.U4tH6ChE1YucabIGUTWdycLJVhsM3hico87drfCFSdQ";
     static apiURL = "https://kenzie-food-api.herokuapp.com";
-    static endpoint = "my/product";
-    // static endpoint = "product";
+    // static endpoint = "my/product";
+    static endpoint = "product";
     static fetchURL = `${this.apiURL}/${this.endpoint}`;
     static requisicao = {headers: {Authorization:`Bearer ${this.token}`}};
     static async get(id = 0) {
